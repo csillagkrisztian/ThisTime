190f44a0be837335ae3ba3ea4cb268f00e6e7ce9
KrisztianCsillag <kcsillag@maxedy.com>
2022-12-29 00:17:42 +0100

    added bottom navigation and editing

--- App.js
+++ App.js
@@ -1,110 +1,64 @@
-import React, { useState, useEffect } from "react";
-import {
-  StyleSheet,
-  Text,
-  View,
-  TouchableOpacity,
-  Dimensions,
-} from "react-native";
-import { Agenda, CalendarProvider } from "react-native-calendars";
-import { FAB, Divider } from "react-native-paper";
+import React, { useState } from "react";
+import { StyleSheet, Text } from "react-native";
+import { BottomNavigation, Provider } from "react-native-paper";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import AgendaRoute from "./components/AgendaRoute";
 
-export default function App() {
-  const testData = {
-    "2022-12-25": [{ name: "Radagon has come", height: 4 }],
-  };
-  const todayString = new Date().toJSON().slice(0, 10);
-  const [items, setItems] = useState(testData);
+const Whiteboard = () => (
+  <SafeAreaView>
+    <Text>Whiteboard</Text>
+  </SafeAreaView>
+);
 
-  useEffect(() => {
-    setItems((state) => {
-      return { [todayString]: [], ...state };
-    });
-  }, []);
+const Lists = () => (
+  <SafeAreaView>
+    <Text>Lists</Text>
+  </SafeAreaView>
+);
 
-  const loadItems = (day) => {
-    const today = day.timestamp + 0 * 24 * 60 * 60 * 1000;
-    const todayString = timeToString(today);
-    if (!items[todayString]) {
-      setItems((state) => {
-        return { [todayString]: [], ...state };
-      });
-    }
-  };
+const MyComponent = () => {
+  const [index, setIndex] = useState(0);
+  const [routes] = useState([
+    {
+      key: "agenda",
+      title: "Agenda",
+      focusedIcon: "calendar-multiple-check",
+      unfocusedIcon: "calendar-blank-multiple",
+    },
+    {
+      key: "whiteboard",
+      title: "Whiteboard",
+      focusedIcon: "clipboard-list",
+      unfocusedIcon: "clipboard-list-outline",
+    },
+    {
+      key: "lists",
+      title: "Lists",
+      focusedIcon: "clipboard-multiple",
+      unfocusedIcon: "clipboard-multiple-outline",
+    },
+  ]);
 
-  const renderItem = (item, isFirst) => {
-    const fontSize = isFirst ? 16 : 14;
-    const color = isFirst ? "black" : "#43515c";
-
-    return (
-      <View style={[styles.container, { marginTop: 20 }]}>
-        <TouchableOpacity onPress={() => console.log(item)}>
-          <Text style={{ fontSize, color }}>{item.name}</Text>
-        </TouchableOpacity>
-        <FAB variant="" icon="plus" onPress={() => console.log("Pressed")} />
-      </View>
-    );
-  };
-
-  const renderEmptyDate = () => {
-    return (
-      <View style={[styles.container, { marginTop: 20 }]}>
-        <Text>{"There are no tasks yet for today!"}</Text>
-        <FAB icon="plus" onPress={() => console.log("Pressed")} />
-      </View>
-    );
-  };
-
-  const rowHasChanged = (r1, r2) => {
-    return r1.name !== r2.name;
-  };
-
-  const timeToString = (time) => {
-    console.log("time", time);
-    const date = new Date(time);
-    return date.toISOString().split("T")[0];
-  };
-
-  const onDayPress = (day) => {
-    const savedItems = Object.entries(items).filter(
-      ([key, value]) =>
-        value.length !== 0 ||
-        (key === todayString && day.dateString === todayString)
-    );
-    setItems(Object.fromEntries(savedItems));
-    loadItems(day);
-  };
+  const renderScene = BottomNavigation.SceneMap({
+    agenda: AgendaRoute,
+    whiteboard: Whiteboard,
+    lists: Lists,
+  });
 
   return (
-    <>
-      <View style={{ flex: 1, marginTop: 30 }}>
-        <CalendarProvider date="">
-          <Agenda
-            onDayPress={onDayPress}
-            items={items}
-            loadItemsForMonth={loadItems}
-            renderItem={renderItem}
-            renderEmptyDate={renderEmptyDate}
-            rowHasChanged={rowHasChanged}
-            showClosingKnob={true}
-            // theme={{calendarBackground: 'red', agendaKnobColor: 'green'}}
-            // renderDay={(day, item) => (<Text>{day ? day.day: 'item'}</Text>)}
-            // showOnlySelectedDayItems
-          />
-        </CalendarProvider>
-        {/*<FAB
-          style={{
-            position: "absolute",
-            left: Dimensions.get("window").width / 1.7,
-            top: Dimensions.get("window").height / 1.2,
-          }}
-          label={"Jump to Today"}
-          onPress={() => {}}
-        ></FAB>*/}
-      </View>
-    </>
+    <Provider>
+      <SafeAreaProvider>
+        <BottomNavigation
+          navigationState={{ index, routes }}
+          onIndexChange={setIndex}
+          renderScene={renderScene}
+        />
+      </SafeAreaProvider>
+    </Provider>
   );
-}
+};
+
+export default MyComponent;
 
 const styles = StyleSheet.create({
   container: {
---
+++ components/AgendaRoute.js
@@ -0,0 +1,181 @@
+import React, { useState, useEffect } from "react";
+import { Agenda, CalendarProvider } from "react-native-calendars";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import {
+  FAB,
+  TextInput,
+  Button,
+  Paragraph,
+  Dialog,
+  Portal,
+  Provider,
+} from "react-native-paper";
+
+export default function AgendaRoute() {
+  const testData = {
+    "2022-12-25": [{ name: "Radagon has come", id: 1576996323453 }],
+  };
+  const todayString = new Date().toJSON().slice(0, 10);
+  const [items, setItems] = useState(testData);
+  const [currentDate, setCurrentDate] = useState(todayString);
+  const [editMode, setEditMode] = useState(false);
+  const [selectedItem, setSelectedItem] = useState({});
+
+  useEffect(() => {
+    setItems((state) => {
+      return { [todayString]: [], ...state };
+    });
+  }, []);
+
+  const loadItems = (day) => {
+    const today = day.timestamp + 0 * 24 * 60 * 60 * 1000;
+    const todayString = timeToString(today);
+    if (!items[todayString]) {
+      setItems((state) => {
+        return { [todayString]: [], ...state };
+      });
+    }
+  };
+
+  const addItem = () => {
+    setItems((state) => ({
+      ...state,
+      [currentDate]: [...state[currentDate], { height: 4, name: "New Item" }],
+    }));
+  };
+
+  const renderItem = (item, isFirst) => {
+    const fontSize = isFirst ? 16 : 14;
+    const color = isFirst ? "black" : "#43515c";
+
+    return (
+      <View style={[styles.container, { marginTop: 20 }]}>
+        <TouchableOpacity onPress={() => console.log(item)}>
+          <Text style={{ fontSize, color }}>{item.name}</Text>
+        </TouchableOpacity>
+        <View style={{ flexDirection: "row" }}>
+          <FAB
+            variant=""
+            icon="pencil"
+            onPress={() => {
+              setSelectedItem(item);
+              setEditMode(true);
+            }}
+          />
+          {isFirst && <FAB variant="" icon="plus" onPress={addItem} />}
+        </View>
+      </View>
+    );
+  };
+
+  const renderEmptyDate = (day) => {
+    return (
+      <View style={[styles.container, { marginTop: 20 }]}>
+        <Text>{"There are no tasks yet for today!"}</Text>
+        <FAB icon="plus" onPress={() => console.log("Pressed")} />
+      </View>
+    );
+  };
+
+  const rowHasChanged = (r1, r2) => {
+    return r1.name !== r2.name;
+  };
+
+  const timeToString = (time) => {
+    const date = new Date(time);
+    return date.toISOString().split("T")[0];
+  };
+
+  const onDayPress = (day) => {
+    const savedItems = Object.entries(items).filter(
+      ([key, value]) =>
+        value.length !== 0 ||
+        (key === todayString && day.dateString === todayString)
+    );
+    setItems(Object.fromEntries(savedItems));
+    setCurrentDate(timeToString(day.timestamp + 0 * 24 * 60 * 60 * 1000));
+    loadItems(day);
+  };
+
+  console.log("editMode", editMode);
+
+  const EditDialog = () => {
+    const [editText, setEditText] = useState(selectedItem.name);
+    return (
+      <Portal>
+        <Dialog
+          visible={editMode}
+          onDismiss={() => {
+            setEditMode(false);
+          }}
+        >
+          <Dialog.Content>
+            <TextInput value={editText} onChangeText={setEditText}></TextInput>
+          </Dialog.Content>
+          <Dialog.Actions>
+            <Button
+              onPress={() => {
+                setEditMode(false);
+                const unselectedItems = items[currentDate].filter(
+                  (item) => item.id !== selectedItem.id
+                );
+                const editedItem = {
+                  ...selectedItem,
+                  name: editText,
+                };
+                setItems((state) => ({
+                  ...state,
+                  [currentDate]: [...unselectedItems, editedItem],
+                }));
+              }}
+            >
+              Done
+            </Button>
+          </Dialog.Actions>
+        </Dialog>
+      </Portal>
+    );
+  };
+
+  return (
+    <>
+      <View style={{ flex: 1, marginTop: 30 }}>
+        <EditDialog></EditDialog>
+        <CalendarProvider date="">
+          <Agenda
+            onDayPress={onDayPress}
+            items={items}
+            loadItemsForMonth={loadItems}
+            renderItem={renderItem}
+            renderEmptyDate={renderEmptyDate}
+            rowHasChanged={rowHasChanged}
+            showClosingKnob={true}
+            // theme={{calendarBackground: 'red', agendaKnobColor: 'green'}}
+            // renderDay={(day, item) => (<Text>{day ? day.day: 'item'}</Text>)}
+            // showOnlySelectedDayItems
+          />
+        </CalendarProvider>
+        {/*<FAB
+              style={{
+                position: "absolute",
+                left: Dimensions.get("window").width / 1.7,
+                top: Dimensions.get("window").height / 1.2,
+              }}
+              label={"Jump to Today"}
+              onPress={() => {}}
+            ></FAB>*/}
+      </View>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "space-between",
+    alignItems: "center",
+    flexDirection: "row",
+    borderRadius: 5,
+    marginRight: 10,
+  },
+});
